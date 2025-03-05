b3ba665380fa27982c980bfb01802babc4aa47b8
SumitJamdade <sumitjamdade2006@gmail.com>
2025-03-05 04:54:01 +0000

    mul using for

---
+++ Week4/Node4.js
@@ -0,0 +1,7 @@
+// Using for loop to print multiplication table of 5
+let num = 5;
+
+console.log(`Multiplication table of ${num} using for loop:`);
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
