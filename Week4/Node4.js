2d6fafd3b1b2c3603fab43c958278c1c54679786
SumitJamdade <sumitjamdade2006@gmail.com>
2025-03-05 05:06:50 +0000

    done

--- Week4/Node4.js
+++ Week4/Node4.js
@@ -1,16 +1,16 @@
 // Using for loop to print multiplication table of 5
-let num = 5;
+let n= 5;
 
 console.log(`Multiplication table of ${num} using for loop:`);
 for (let i = 1; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num * i}`);
+    console.log(`${n} x ${i} = ${n * i}`);
 }
 // Using while loop to print multiplication table of 5
-let num = 5;
+let n= 5;
 let i = 1;
 
-console.log(`Multiplication table of ${num} using while loop:`);
+console.log(`Multiplication table of ${n} using while loop:`);
 while (i <= 10) {
-    console.log(`${num} x ${i} = ${num * i}`);
+    console.log(`${n} x ${i} = ${n * i}`);
     i++; // Increment i to move to the next iteration
 }
