baa5185407dee7e872cebb12a502e37f78ec407b
SumitJamdade <sumitjamdade2006@gmail.com>
2025-03-05 04:55:07 +0000

    while loop mul

--- Week4/Node4.js
+++ Week4/Node4.js
@@ -5,3 +5,12 @@ console.log(`Multiplication table of ${num} using for loop:`);
 for (let i = 1; i <= 10; i++) {
     console.log(`${num} x ${i} = ${num * i}`);
 }
+// Using while loop to print multiplication table of 5
+let num = 5;
+let i = 1;
+
+console.log(`Multiplication table of ${num} using while loop:`);
+while (i <= 10) {
+    console.log(`${num} x ${i} = ${num * i}`);
+    i++; // Increment i to move to the next iteration
+}
