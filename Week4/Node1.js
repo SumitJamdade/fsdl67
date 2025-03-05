0ac458799cc61315eb47d6838940ea6ca0f25ecc
SumitJamdade <sumitjamdade2006@gmail.com>
2025-03-05 04:47:09 +0000

    done

---
+++ Week4/Node1.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 5;
+
+let sum = x + y; // Addition
+let difference = x - y; // Subtraction
+let product = x * y; // Multiplication
+let quotient = x / y; // Division
+let remainder = x % y; // Modulus (remainder after division)
